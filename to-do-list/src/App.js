8b2f605e1934fdeec355aa5482edd7e4af58a5ad
SongSiYeol11 <e44688928@gmail.com>
2025-12-07 04:26:41 +0000

    commit

---
+++ to-do-list/src/App.js
@@ -0,0 +1,13 @@
+import React, {useState} from 'react';
+import './App.css';
+
+function App() {
+  return (
+   <main>
+    <input type="text"/>
+    <button>추가</button>
+   </main>
+  );
+}
+
+export default App;
